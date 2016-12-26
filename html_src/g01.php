4a323a32c966c9bd6da814d39ac1e74e756b727b
Matthew Bauer <bauer.s.matthew@gmail.com>
2016-12-25 18:20:28 -0800

    Fix email signup box looks

--- html_src/g01.php
+++ html_src/g01.php
@@ -46,15 +46,52 @@
     </p>
 
     <!-- EMAIL FORM -->
-    <p class="col-md-12 textwall">
+    <p class="col-md-12 textwall" id="email-signup">
         Want to recieve email updates? Give us your email below:
     </p>
-    <form class="col-md-6">
-        <div class="form-group">
-            <label for="email" class="sr-only">Email:</label>
-            <input type="email" class="form-control" id="email">
+    <div class="row endbuf">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+            <form method="post" action="#email-signup">
+                <div class="form-group">
+                    <label for="email" class="sr-only">Email:</label>
+                    <input type="email" name="email" class="form-control" placeholder="Enter email...">
+                </div>
+                <button type=submit class="btn btn-default">Submit</button>
+            </form>
         </div>
-        <button type=submit class="btn btn-default">Submit</button>
-    </form>
-
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                if(empty($_POST["email"]))
+                {
+                    print '
+                    <div class="col-md-4 alert alert-danger fade in">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        Oops -- doesn\'t look like you entered anything!
+                    </div>';
+                }
+                else
+                {
+                    // verify
+                    if(!preg_match("/[a-z0-9._-]+@[a-z0-9._-]+/", $_POST["email"]))
+                    {
+                        print '
+                        <div class="col-md-4 alert alert-danger fade in">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            Oops -- that doesn\'t look like an email address...
+                        </div>';
+                    }
+                    else
+                    {
+                        print '
+                        <div class="col-md-4 alert alert-success fade in">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            Success!
+                        </div>';
+                    }
+                }
+            }
+        ?>
+    </div>
 {% endblock %}
