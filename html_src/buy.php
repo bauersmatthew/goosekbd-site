4fefbed877a7dd3b6b6b5bed2818406e6bea36e8
Matthew Bauer <bauer.s.matthew@gmail.com>
2017-01-03 12:41:13 -0800

    Add placeholder buy.php page

---
+++ html_src/buy.php
@@ -0,0 +1,10 @@
+{% extends "templates/page.html" %}
+{% from "templates/macros.html" import navbar %}
+
+{% block title %}Buy{% endblock %}
+
+{% block body %}
+    {{ navbar("products") }}
+
+    <h1 class="text-centered"><br /><br /><br />Coming Soon!</h1>
+{% endblock %}
