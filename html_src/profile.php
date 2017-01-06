e0bb157b68fa741c693a745126cbec79139536c5
Matthew Bauer <bauer.s.matthew@gmail.com>
2017-01-05 18:24:04 -0800

    Fix PHP

--- html_src/profile.php
+++ html_src/profile.php
@@ -24,7 +24,7 @@
     <h2 class="col-md-11 col-md-offset-1" id="orders-title">My Orders</h2>
 
     <div class="row">
-        <div class="col-sm-2 col-sm-offset-1 col-xs-2 col-xs-offset-1"></div>
+        <div class="col-xs-2 col-xs-offset-1"></div>
         <p class="col-xs-2">Order Number</p>
         <p class="col-xs-2">Product x Quantity</p>
         <p class="col-xs-2">Order Date</p>
@@ -64,26 +64,38 @@
         $stmt->execute();
         $stmt->bind_result($order_id, $product_id, $quantity, $status, $date_ordered, $date_statuschange);
 
-        $stmt_img = $conn->prepare(
-            "SELECT name, icon FROM `products` WHERE id = ?");
-        $stmt->bind_param($product_id);
-        $stmt->bind_result($product_name, $product_icon);
-
+        $orders = [];
         while($stmt->fetch())
         {
+            $orders[] = array($order_id, $product_id, $quantity, $status, $date_ordered, $date_statuschange);
+        }
+        $stmt->close();
+
+        $stmt = $conn->prepare(
+            "SELECT name, icon FROM `products` WHERE id = ?");
+        $stmt->bind_param("s", $order_id);
+        $stmt->bind_result($product_name, $product_icon);
+
+        foreach($orders as $order)
+        {
             # get product icon
-            $stmt_img->execute();
-            $stmt_img->fetch();
+            $order_id = $order[0];
+            $stmt->execute();
+            $stmt->fetch();
 
             print '
+            <div class="col-xs-10 col-xs-offset-1 bottom-border"></div>
             <div class="row">
-                <img class="col-sm-1 col-sm-offset-2 col-xs-2 col-xs-offset-1 img-responsive" src="' . $product_icon . '" alt="Product image">
-                <p class="col-xs-1">#' . $order_id . '</p>
-                <p class="col-xs-2">' . $product_name . ' x ' . $quantity . '</p>
-                <p class="col-xs-2">' . $date_ordered . '</p>
-                <p class="col-xs-2">' . translate_status_code($status) . $date_statuschange . '</p>
+                <img class="col-sm-2 col-sm-offset-1 col-xs-2 col-xs-offset-1 img-responsive" src="' . $product_icon . '" alt="Product image">
+                <p class="col-xs-2">#' . $order[0] . '</p>
+                <p class="col-xs-2">' . $product_name . ' x ' . $order[2] . '</p>
+                <p class="col-xs-2">' . $order[4] . '</p>
+                <p class="col-xs-2">' . translate_status_code($order[3]) . $order[5] . '</p>
             </div>';
         }
+
+        $stmt->close();
+        $conn->close();
     ?>
 
 {% endblock %}
