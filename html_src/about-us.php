6faa2ed55f77df20a915d1d97a397707a4f137be
Matthew Bauer <bauer.s.matthew@gmail.com>
2017-01-06 00:03:06 -0800

    Containerify about-us.php

--- html_src/about-us.php
+++ html_src/about-us.php
@@ -6,12 +6,14 @@
 {% block body %}
     {{ navbar("about us") }}
 
-    <div class="col-md-12 textwall endbuf">
-        Goose Keyboard Co. is composed of
-        Siddhardha Maligireddy (/u/misterbinlee)
-        Nikolai Oh (/u/t-mootypus),
-        and Matthew Bauer (/u/cyangekko).
-        <br /><br />
-        To contact us, shoot an email to: <code>support@goosekbd.com</code>
+    <div class="container-fluid">
+        <div class="col-md-12 textwall endbuf">
+            Goose Keyboard Co. is composed of
+            Siddhardha Maligireddy (/u/misterbinlee)
+            Nikolai Oh (/u/t-mootypus),
+            and Matthew Bauer (/u/cyangekko).
+            <br /><br />
+            To contact us, shoot an email to: <code>support@goosekbd.com</code>
+        </div>
     </div>
 {% endblock %}
